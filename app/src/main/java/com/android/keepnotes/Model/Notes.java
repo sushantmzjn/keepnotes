babc124563b5305cf472e81e36999d38a1ee903b
sushantmzjn <sushantmaharjan08@gmail.com>
2022-07-09 12:31:49 +0545

    Keep notes

---
+++ app/src/main/java/com/android/keepnotes/Model/Notes.java
@@ -0,0 +1,30 @@
+package com.android.keepnotes.Model;
+
+public class Notes {
+    int ID;
+    String title, description;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
